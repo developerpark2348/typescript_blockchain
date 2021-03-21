dc2afe3a529bc1f8875b2891d3c6902bc63f120b
developerpark2348 <developerpark2348@gmail.com>
2021-03-21 19:43:02 +0900

    typescript에서 class사용시
    interface사용시 index.js에는 interface가 옮겨지지 않지만,
    class사용시 class는 전달된다.

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const person = {
-    name: "jinwoo",
-    age: 22,
-    gender: "male"
-};
+class Human {
+    constructor(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+const lynn = new Human("Lynn", 12, "female");
 const sayHi = (person) => {
     return `Hello ${person.name}, you are a ${person.age} old and you are a ${person.gender}!`;
 };
 // console.log였다면, 반환값이 없기에 void를 함수의 결과값으로 줘야함.
-console.log(sayHi(person));
+console.log(sayHi(lynn));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,21 @@
-interface Human {
-    name: string;
-    age: number;
-    gender: string;
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name: string, age: number, gender: string){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
 
-const person = {
-    name: "jinwoo",
-    age: 22,
-    gender: "male"
-};
+const lynn = new Human("Lynn", 12, "female")
 
 const sayHi = (person: Human):string => {
     return `Hello ${person.name}, you are a ${person.age} old and you are a ${person.gender}!`;
 }
 // console.log였다면, 반환값이 없기에 void를 함수의 결과값으로 줘야함.
 
-console.log(sayHi(person));
+console.log(sayHi(lynn));
 
 export {}; 
