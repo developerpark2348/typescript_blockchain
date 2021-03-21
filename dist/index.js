733916bd8f34582fac7fcd12034b9ab8069171d6
developerpark2348 <developerpark2348@gmail.com>
2021-03-21 19:35:22 +0900

    ts내 배열 사용
    interface 인터페이스명 {
    name: string;
    age: number;
    gender: string;
    }
    => 인터페이스를 먼저 설정한다.
    
    const 배열명 = {
    name: "jinwoo"
    age: 22
    gendeR: "male"
    };
    
    const 함수명 = (파라미터: 인터페이스명):string => {
    대충 함수들~
    }
    
    함수명(배열명)
    => 파라미터에 인터페이스를 넣었기에, 함수명()내에 배열을 넣을수있게된다. 단, 배열내의 string, number등은 똑같아야함.

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const sayHi = (name, age, gender) => {
-    return `Hello ${name}, you are a ${age} old and you are a ${gender}!`;
+const person = {
+    name: "jinwoo",
+    age: 22,
+    gender: "male"
+};
+const sayHi = (person) => {
+    return `Hello ${person.name}, you are a ${person.age} old and you are a ${person.gender}!`;
 };
 // console.log였다면, 반환값이 없기에 void를 함수의 결과값으로 줘야함.
-console.log(sayHi("Park", 22, "male"));
+console.log(sayHi(person));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
-const sayHi = (name:string, age:number, gender:string):string => {
-    return `Hello ${name}, you are a ${age} old and you are a ${gender}!`;
+interface Human {
+    name: string;
+    age: number;
+    gender: string;
+}
+
+const person = {
+    name: "jinwoo",
+    age: 22,
+    gender: "male"
+};
+
+const sayHi = (person: Human):string => {
+    return `Hello ${person.name}, you are a ${person.age} old and you are a ${person.gender}!`;
 }
 // console.log였다면, 반환값이 없기에 void를 함수의 결과값으로 줘야함.
 
-console.log(sayHi("Park", 22, "male"));
+console.log(sayHi(person));
 
 export {}; 
