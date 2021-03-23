22f56b1727d8a487ae144efa678ade1d8f795395
developerpark2348 <developerpark2348@gmail.com>
2021-03-23 14:58:58 +0900

    done
    done

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const CryptoJS = require("crypto-js");
 class Block {
     constructor(index, hash, previousHash, data, timestamp) {
         this.index = index;
@@ -9,7 +10,24 @@ class Block {
         this.timestamp = timestamp;
     }
 }
+Block.calculateBlockHash = (index, previousHash, timestamp, data) => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 const genesisBlock = new Block(0, "20210323", "", "Hi", 126);
 let blockchain = [genesisBlock];
-console.log(blockchain);
+//genesisBlock을 blockchain변수에 넣음.
+const getBlockchain = () => blockchain;
+// blockChain을 가져오는 함수
+const getLatestBlock = () => blockchain[blockchain.length - 1];
+// 가장 최근의 블록을 가져오기 위한 함수
+const getNewTimeStamp = () => Math.round(new Date().getTime() / 1000);
+// 시간을 알기위해 제작한 함수
+const createNewBlock = (data) => {
+    const previousBlock = getLatestBlock();
+    const newIndex = previousBlock.index + 1;
+    const newTimestamp = getNewTimeStamp();
+    const newHash = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimestamp, data);
+    const newBlock = new Block(newIndex, newHash, previousBlock.hash, data, newTimestamp);
+    return newBlock;
+};
+//각각의 데이터를 넣어 블락을 만드는 함수
+console.log(createNewBlock("Hello"), createNewBlock("Bye"));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,12 @@ class Block {
     public data: string;
     public timestamp: number;
 
-    static calculateBlcokHash = (index:number, previousHash:string, timestamp:number, data:string):string => 
+    static calculateBlockHash = (
+        index:number, 
+        previousHash:string, 
+        timestamp:number, 
+        data:string
+    ):string => 
     CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 
     constructor(
@@ -28,11 +33,38 @@ class Block {
 const genesisBlock:Block = new Block(0, "20210323", "", "Hi", 126)
 
 let blockchain: Block[] = [genesisBlock];
+//genesisBlock을 blockchain변수에 넣음.
 
 const getBlockchain = () : Block[] => blockchain;
+// blockChain을 가져오는 함수
 
-const getLatestBlcok = () : Block => getBlockchain[blockchain.length - 1];
+const getLatestBlock = () : Block => blockchain[blockchain.length - 1];
+// 가장 최근의 블록을 가져오기 위한 함수
 
 const getNewTimeStamp = () : number => Math.round(new Date().getTime() / 1000);
+// 시간을 알기위해 제작한 함수
+
+const createNewBlock = (data:string) : Block => {
+    const previousBlock : Block  = getLatestBlock();
+    const newIndex : number = previousBlock.index + 1;
+    const newTimestamp : number = getNewTimeStamp();
+    const newHash : string = Block.calculateBlockHash(
+        newIndex, 
+        previousBlock.hash, 
+        newTimestamp, 
+        data
+    );
+    const newBlock : Block = new Block(
+        newIndex, 
+        newHash, 
+        previousBlock.hash, 
+        data, 
+        newTimestamp
+    );
+    return newBlock;
+}
+//각각의 데이터를 넣어 블락을 만드는 함수
+
+console.log(createNewBlock("Hello"), createNewBlock("Bye"))
 
 export {}; 
